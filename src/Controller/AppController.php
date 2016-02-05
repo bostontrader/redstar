ba2a0ba2559d269a7df25f089f9049713b562a84
tradloff <bostontrader@gmail.com>
2016-02-06 07:27:15 +0800

    Inital footprint for layout and home page.

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,62 +1,81 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link      http://cakephp.org CakePHP(tm) Project
- * @since     0.2.9
- * @license   http://www.opensource.org/licenses/mit-license.php MIT License
- */
+
 namespace App\Controller;
 
 use Cake\Controller\Controller;
 use Cake\Event\Event;
+//use Cake\I18n\I18n;
+//use Cake\ORM\TableRegistry;
 
-/**
- * Application Controller
- *
- * Add your application-wide methods in the class below, your controllers
- * will inherit them.
- *
- * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
- */
-class AppController extends Controller
-{
+class AppController extends Controller {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * e.g. `$this->loadComponent('Security');`
-     *
-     * @return void
-     */
-    public function initialize()
-    {
+    public function initialize() {
         parent::initialize();
 
-        $this->loadComponent('RequestHandler');
         $this->loadComponent('Flash');
+        /*$this->loadComponent(
+            'Auth', [
+                'logoutRedirect' => [
+                    'controller' => 'Pages',
+                    'action' => 'display',
+                    'home'
+                ],
+                'authorize' => 'Controller',
+                'unauthorizedRedirect' => '/'  // authenticated, but unauthorized
+            ]
+        );*/
+
+        // Now set role flags
+        /*$user_id=$this->Auth->user('id');
+        if(!is_null($user_id)) {
+            $users = TableRegistry::get('Users');
+            $user=$users->get($user_id, ['contain' => ['Roles']]);
+            $this->isAdmin = false;
+            $this->isTeacher = false;
+            foreach($user->roles as $role) {
+                if($role->title=='admin') $this->isAdmin=true;
+                if($role->title=='teacher') $this->isTeacher=true;
+            }
+        }*/
+
+        // The language is initially set in bootstrap.php.  If we want to change the language on-the-fly,
+        // such as in clicking the little flags, then we need to store the desired language in the session,
+        // retrieve that value here, and use that value to override the default language.  Even though the
+        // default in bootstrap.php is en_EN, we still need to set it here.
+        $lang=$this->request->session()->read('Config.language');
+        switch($lang) {
+            case "zh_CN":
+                I18n::locale('zh_CN');
+                break;
+            case "zh_PN":
+                I18n::locale('zh_PN');
+                break;
+            case "en_US":
+                I18n::locale('en_US');
+                break;
+        }
     }
 
+    // Nothing is authorized unless a controller says so.
+    // Admin is always authorized.
+    /*public function isAuthorized($userArray) {
+        $users = TableRegistry::get('Users');
+        $user=$users->get($userArray['id'], ['contain' => ['Roles']]);
+        foreach($user->roles as $role) {
+            if($role->title=='admin') return true;
+        }
+        return false;
+    }*/
+
     /**
-     * Before render callback.
      *
      * @param \Cake\Event\Event $event The beforeRender event.
      * @return void
      */
-    public function beforeRender(Event $event)
-    {
-        if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
-        ) {
-            $this->set('_serialize', true);
-        }
+    public function beforeRender(Event $event) {
+        //$this->set('currentUser',$this->Auth->user());
+        $this->set('currentUser','admin');
+        //$this->set('isAdmin',$this->isAdmin);
+        $this->set('isAdmin',true);
     }
 }
--- src/Template/Layout/default.ctp
+++ src/Template/Layout/default.ctp
@@ -1,57 +1,63 @@
-<?php
-/**
- * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- *
- * Licensed under The MIT License
- * For full copyright and license information, please see the LICENSE.txt
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
- * @link          http://cakephp.org CakePHP(tm) Project
- * @since         0.10.0
- * @license       http://www.opensource.org/licenses/mit-license.php MIT License
- */
-
-$cakeDescription = 'CakePHP: the rapid development php framework';
-?>
 <!DOCTYPE html>
 <html>
 <head>
-    <?= $this->Html->charset() ?>
+    <?= $this->Html->charset(); ?>
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>
-        <?= $cakeDescription ?>:
-        <?= $this->fetch('title') ?>
-    </title>
-    <?= $this->Html->meta('icon') ?>
+    <title>Redstar</title>
 
-    <?= $this->Html->css('base.css') ?>
-    <?= $this->Html->css('cake.css') ?>
+    <? // If you are using the CSS version, only link these 2 files, you may add app.css to use for your overrides if you like ?>
+    <?= $this->Html->css('normalize.css'); ?>
+    <?= $this->Html->css('foundation.css'); ?>
+    <!--
+    <script src="js/vendor/modernizr.js"></script> -->
 
-    <?= $this->fetch('meta') ?>
-    <?= $this->fetch('css') ?>
-    <?= $this->fetch('script') ?>
 </head>
 <body>
-    <nav class="top-bar expanded" data-topbar role="navigation">
-        <ul class="title-area large-3 medium-4 columns">
+
+    <?php //if($currentUser) {
+        //$userMsg   = "current user = " . $currentUser['username'];
+        $userMsg='';
+        //$loginLink = $this->Html->link(
+            //'Logout',
+            //'/users/logout',
+            //['class' => 'button']
+        //);
+        $loginLink='';
+
+    //} else {
+        //$userMsg   = "not logged in";
+        //$loginLink = $this->Html->link(
+            //__('Login'),
+            //'/users/login',
+            //['class' => 'button']
+        //);
+    //}
+    ?>
+
+
+    <nav class="top-bar" data-topbar role="navigation">
+        <ul class="title-area">
             <li class="name">
-                <h1><a href=""><?= $this->fetch('title') ?></a></h1>
+                <h1><a href="#">Redstar</a></h1>
             </li>
         </ul>
+
         <section class="top-bar-section">
+            <!-- Right Nav Section -->
             <ul class="right">
-                <li><a target="_blank" href="http://book.cakephp.org/3.0/">Documentation</a></li>
-                <li><a target="_blank" href="http://api.cakephp.org/3.0/">API</a></li>
+                <li><a href="#"><?= $userMsg ?></a></li>
+                <li><a href="#"><?= $loginLink ?></a></li>
+
             </ul>
+
         </section>
     </nav>
-    <?= $this->Flash->render() ?>
-    <section class="container clearfix">
-        <?= $this->fetch('content') ?>
-    </section>
-    <footer>
-    </footer>
+
+    <?php
+        //if($currentUser) {
+        echo $this->fetch('content');
+        //}
+    ?>
+
 </body>
 </html>
